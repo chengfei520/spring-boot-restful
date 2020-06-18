3ca87ffe0d12243bab6f78c5a125937ff01b7b63
chengfei <chengfei@qq.com>
2020-06-18 17:12:38 +0800

    增删改员工（修改和删除分别使用PUT和DELETE请求，需要在配置文件中打开）

--- src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.atguigu.springboot.controller;
 
+import com.atguigu.springboot.dao.DepartmentDao;
 import com.atguigu.springboot.dao.EmployeeDao;
+import com.atguigu.springboot.entities.Department;
 import com.atguigu.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -13,11 +15,54 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
         return "emp/list";
+    }
+    //到添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //页面要显示所有部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //添加员工
+    //springMVC自动将请求参数和入参对象的属性一一绑定，要求请求参数名字和JavaBean入参的对象里面的属性名相同
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+        employeeDao.save(employee);
+        //这是重定向，如果以forward：开始，就是请求转发
+        return "redirect:/emps";
+    }
+    //来到修改页面，查出当前员工，在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        //页面要显示所有部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //员工修改
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        System.out.println(employee);
+        employeeDao.save(employee);
+
+        return "redirect:/emps";
+    }
+    //员工删除
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
